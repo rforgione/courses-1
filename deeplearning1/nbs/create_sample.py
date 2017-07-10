d0b6cbea9bee8cefd9ea5923d0fbb6f3053a23cd
Ubuntu <ubuntu@ip-10-0-0-6.us-west-2.compute.internal>
2017-07-10 01:27:25 +0000

    rewrite sample creation script

--- deeplearning1/nbs/create_sample.py
+++ deeplearning1/nbs/create_sample.py
@@ -6,13 +6,34 @@ import shutil
 sample_pct = .25
 random.seed(2)
 
-for i in os.listdir("data/train"):
-	num = random.randint(1,(1/sample_pct))
-	if num == 1:
-		shutil.copy("data/train/%s" % i, "data_sample/train/%s" % i)
+dirs = ['data_sample', 
+	'data_sample/train', 
+	'data_sample/train/dogs',
+	'data_sample/train/cats',
+	'data_sample/valid', 
+	'data_sample/valid/dogs',
+	'data_sample/valid/cats']
 
-for i in os.listdir("data/valid"):
+for d in dirs:
+	if not os.path.exists(d): os.mkdir(d)
+
+for i in os.listdir("data/train/dogs"):
 	num = random.randint(1,(1/sample_pct))
 	if num == 1:
-		shutil.copy("data/valid/%s" % i, "data_sample/valid/%s" % i)
+		shutil.copy("data/train/dogs/%s" % i, "data_sample/train/dogs")
+
+for i in os.listdir("data/valid/dogs"):
+	num = random.randint(1,(1/sample_pct))
+	if num == 1:
+		shutil.copy("data/valid/dogs/%s" % i, "data_sample/valid/dogs")
+
+for i in os.listdir("data/train/cats"):
+	num = random.randint(1,(1/sample_pct))
+	if num == 1:
+		shutil.copy("data/train/cats/%s" % i, "data_sample/train/cats")
+
+for i in os.listdir("data/valid/cats"):
+	num = random.randint(1,(1/sample_pct))
+	if num == 1:
+		shutil.copy("data/valid/cats/%s" % i, "data_sample/valid/cats")
 
